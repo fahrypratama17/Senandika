b3cb9a593180e2e9a6a75831b6f55dc7635f88fb
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-11-11 17:35:49 +0700

    feat: add ButtonTry component and integrate it into HeadlineSection

---
+++ src/features/home/hero-section/components/button-try.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/shared/components/ui/button";
+
+export default function ButtonTry() {
+  return (
+    <Button className="bg-[#dcc0ec] w-20 h-10">
+      Try
+    </Button>
+  )
+}
--- src/features/home/hero-section/components/headline-section.tsx
+++ src/features/home/hero-section/components/headline-section.tsx
@@ -1,8 +1,14 @@
+import ButtonTry from "./button-try"
+
 export default function HeadlineSection() {
   return (
     <section className="flex flex-col min-h-[80vh] items-center justify-center gap-20">
       <h1 className="text-[56px]">Create A Future Where You Can Feel</h1>
       <h3 className="text-[36px] text-center">At Ease</h3>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <p>Try Our ChatBot Now</p>
+        <ButtonTry />
+      </div>
     </section>
   )
 }
